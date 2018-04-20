f5ea54ecc7fdf0c6a969ef52f105444f0c120ed9
jrm51c <jmccarthyn2n@yahoo.com>
2018-04-20 09:25:06 -0500

    added dynamic URL creation based upon button being clicked

--- assets/javascript/giphyAPI.js
+++ assets/javascript/giphyAPI.js
@@ -3,33 +3,52 @@ $(document).ready(function() {
   //=============================================================================================
 
   var apiKey = "Z0vmrS1H3c1iLslkkHyHnqQ7KAkLxnFe";
-  var topics = ["Incredible Hulk","Thor","Iron Man","Abomination"];
+  var topics = ["hulk","thor","iron man","abomination"];
+  var topic = "";
   console.log(topics);
 
         // Function for dumping the JSON content for each button into the div
-        function displayTopicImages() {
-
-           // Storing our giphy API URL for a random cat image
-       var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=thor";
+       function displayTopicImages() {
+         var index = $(this).attr("index");
+         console.log("index" + index);
+         var queryString = topics[index];
       //var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=Z0vmrS1H3c1iLslkkHyHnqQ7KAkLxnFe&tag=hulk";
-      //var queryURL = "http://api.giphy.com/v1/gifs/search?q=hulk&api_key=dc6zaTOxFJmzC&limit=1"
-      //var queryURL = "https://api.giphy.com/v1/gifs/search?q=hulk&api_key=dc6zaTOxFJmzC";
+      var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Z0vmrS1H3c1iLslkkHyHnqQ7KAkLxnFe&q=" + queryString + "&limit=10";
+      //var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Z0vmrS1H3c1iLslkkHyHnqQ7KAkLxnFe&q=" + queryString + "&limit=10"
 
-          console.log(queryURL);
-          var topic = $(this).attr("data-name");
-          console.log(topic);
-         // var queryURL = "https://www.omdbapi.com/?t=" + movie + "&plot=short&apikey=trilogy";
-          var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + apiKey + "&limit=1";
-          console.log(queryURL);
-  
-          $.ajax({
-            url: queryURL,
-            method: "GET"
-          }).then(function(response) {
-            $("#topics-view").text(JSON.stringify(response));
-            renderButtons();
-          });
-        }    
+      // Perfoming an AJAX GET request to our queryURL
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+      // After the data from the AJAX request comes back
+        .then(function(response) {
+          console.log("response: ", response);
+          console.log("response.data: ", response.data);
+          console.log("response.data[0]: ", response.data[0]);
+          //console.log("response.data[0].images: ", response.data[0].images);
+          //console.log("response.data[0].images.original: ", response.data[0].images.original);
+          //console.log("response.data[0].images.original.url: ", response.data[0].images.original.url);
+
+        // Saving the image_original_url property
+          for (i = 0; i < 10; i++) {
+          //var imageUrl = response.data[i].images.original.url;
+          var imageUrl = response.data[i].images.original_still.url;
+
+          // Creating and storing an image tag
+          var topicImage = $("<img>");
+
+          // Setting the catImage src attribute to imageUrl
+          topicImage.attr("src", imageUrl);
+          topicImage.attr("alt", "topic image");
+
+          // Prepending the image to the images div
+          $("#images").prepend(topicImage);
+        }
+         
+        });
+      }; 
 
   // Function for displaying topic images
         function renderButtons() {
@@ -46,30 +65,41 @@ $(document).ready(function() {
             var a = $("<button>");
             // Adding a class of topic to our button
             a.addClass("topic");
+            // adding an id
+            a.attr("id", "btn" + i);
+            // adding th eindex
+            a.attr("index", i);
             // Adding a data-attribute
-            a.attr("data-name", topics[i]);
+            a.attr("topic-search", topics[i]);
             // Providing the initial button text
             a.text(topics[i]);
             // Adding the button to the buttons-view div
             $("#buttons-view").append(a);
           }
+          console.log(a);
         }
 
-              // Function for adding a new topic entered by user
+
+
+        // Function for adding a new topic entered by user
               $("#add-topic").on("click", function(event) {
               event.preventDefault();
 
         // This line grabs the input from the textbox
-        var topic = $("#topic-input").val().trim();
-      
-          if (topics.includes(topic)) {
-            alert("fuck");
-            // Clear topic entered by user
+        topic = $("#topic-input").val().trim();
+        var lowerCaseTopic = topic.toLowerCase();
+        console.log("topic" + topic);
+          if (lowerCaseTopic === "") {
+            alert("Enter a Valid Topic");
+            // Clear text input
             $("#topic-input").val("");
-          }
-          else  {
+          }   else if (topics.includes(lowerCaseTopic)) {
+            alert("Topic Already Exists");
+            // Clear text input
+            $("#topic-input").val("");
+          } else {
             // Adding the topic from the textbox to our array
-            topics.push(topic);
+            topics.push(lowerCaseTopic);
             // Calling renderButtons which handles the processing of our topics array
             renderButtons();
             // Clear topic entered by user
@@ -80,4 +110,9 @@ $(document).ready(function() {
 
       // Calling the renderButtons function to display the initial buttons
       renderButtons();
-});  
+     
+
+      // Using $(document).on instead of $(".movie").on to add event listeners to dynamically generated elements
+     $(document).on("click", ".topic", displayTopicImages);
+
+});
